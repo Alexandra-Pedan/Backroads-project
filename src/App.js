dcd31761241ad677e12f33f2a2f9ab61e3a553ad
Aleksandra Pedan <sashka28465@gmail.com>
2023-06-12 15:52:24 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Tours from './components/Tours'
+import Footer from './components/Footer'
+import Services from './components/Services'
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  )
+}
+export default App
---
+++ src/components/SocialLink.js
@@ -0,0 +1,11 @@
+function SocialLink({ href, icon, itemClass }) {
+  return (
+    <li>
+      <a href={href} className={itemClass} target="_blank" rel="noreferrer">
+        <i className={icon}></i>
+      </a>
+    </li>
+  )
+}
+
+export default SocialLink
